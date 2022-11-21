6315ca32dee6b2ed4a9b6e36e9aec56c6ccfe9c9
rickymangerie <ricky@drexel.edu>
2022-11-20 20:40:17 -0500

    added some tests
    added display and hide item adder, test adding items to list

--- src/test/java/edu/drexel/se320/SeleniumTest.java
+++ src/test/java/edu/drexel/se320/SeleniumTest.java
@@ -14,39 +14,154 @@ import org.junit.jupiter.api.Test;
 
 public class SeleniumTest {
 
-    // UPDATE THIS PATH
+    // PATH RELATIVE TO MY LOCAL MACHINE
     protected final String uiPath = "file:///I:/My%20Drive/Drexel/Fall%202022%20(202315)/SE%20320/Homeworks/HW5/homework-5-rmm429/web/index.html";
+
+    // the max amount of items that can be added to a list
+    protected final int MAX_ITEMS = 3;
+
+//    @Test
+//    public void testOneItem() {
+//        WebDriver driver = new FirefoxDriver();
+//        try {
+//            driver.get(uiPath);
+//            // Find the + to click to display the form to add a to-do
+//            // Looking up by the id, not the name attribute
+//            WebElement elt = driver.findElement(By.id("controls1plus"));
+//
+//            // Click on the [+]
+//            elt.click();
+//
+//            // Find the form field
+//            WebElement input = driver.findElement(By.id("itemtoadd"));
+//
+//            // Make up a to-do
+//            input.sendKeys("Something to do");
+//
+//            // Find and click the "Add to list" button
+//            WebElement addButton = driver.findElement(By.id("addbutton"));
+//            addButton.click();
+//
+//            /* The first element added to the list will have id "item1"
+//             * Subsequent list items will have IDs item2, item3, etc.
+//             * Arguably this is too brittle, but rather than forcing you
+//             * all to become experts on the DOM, you may assume this is done
+//             * correctly, and/or you're testing this functionality implicitly. */
+//            WebElement li = driver.findElement(By.id("item1"));
+//            // We use startsWith because getText includes the text of the Delete button
+//            assertTrue(li.getText().startsWith("Something to do"), "Checking correct text for added element");
+//        } finally {
+//            driver.quit();
+//        }
+//    }
+
+    // throws InterruptedException
+    // Thread.sleep(3000);
+
+    /*
+     * Test 1a: Test Controls Revealed
+     * This will test that when the [+] icon is clicked,
+     * the controls to add a to-do are displayed
+     */
+    @Test
+    public void testRevealControls() {
+
+        WebDriver driver = new FirefoxDriver();
+
+        try {
+
+            driver.get(uiPath);
+
+            // Find the [+] icon to click to display the form to add a to-do
+            WebElement plus = driver.findElement(By.id("controls1plus"));
+            // Click on the [+] icon
+            plus.click();
+
+            // Find the form that adds a to-do
+            WebElement controls = driver.findElement(By.id("controls1"));
+
+            // Ensure that after clicking the [+] icon, the form is revealed
+            assertTrue(controls.isDisplayed(), "Checking that the controls are displayed after clicking the [+] icon");
+
+        } finally {
+            driver.quit();
+        }
+
+    }
+
+    /*
+     * Test 1b: Test Controls Hidden
+     * This will test that when the [-] icon is clicked
+     * after the [+] is clicked to display the controls to add a to-do,
+     * said controls are then hidden
+     */
+    @Test
+    public void testHideControls() {
+
+        WebDriver driver = new FirefoxDriver();
+
+        try {
+
+            driver.get(uiPath);
+
+            // Find the [+] icon to click to display the form to add a to-do
+            WebElement plus = driver.findElement(By.id("controls1plus"));
+            // Click on the [+] icon
+            plus.click();
+
+            // Find the [-] icon to click to hide the form to add a to-do
+            WebElement minus = driver.findElement(By.id("controls1minus"));
+            // Click on the [-] icon
+            minus.click();
+
+            // Find the form that adds a to-do
+            WebElement controls = driver.findElement(By.id("controls1"));
+
+            // Ensure that after clicking the [-] icon, the form is hidden
+            assertFalse(controls.isDisplayed(), "Checking that the controls are hidden after clicking the [-] icon");
+
+        } finally {
+            driver.quit();
+        }
+
+    }
 
     @Test
     public void testOneItem() {
-        WebDriver driver = new FirefoxDriver();
-        try {
-            driver.get(uiPath);
-            // Find the + to click to display the form to add a todo
-            // Looking up by the id, not the name attribute
-            WebElement elt = driver.findElement(By.id("controls1plus"));
 
-            // Click on the [+]
+        WebDriver driver = new FirefoxDriver();
+
+        try {
+
+            driver.get(uiPath);
+
+            // Find the [+] to click to display the form to add a to-do
+            WebElement elt = driver.findElement(By.id("controls1plus"));
+            // Click on the [+] icon
             elt.click();
 
-            // Find the form field
+            // Find the form field and "Add to list" button
             WebElement input = driver.findElement(By.id("itemtoadd"));
-
-            // Make up a todo
-            input.sendKeys("Something to do");
-
-            // Find and click the "Add to list" button
             WebElement addButton = driver.findElement(By.id("addbutton"));
-            addButton.click();
 
-            /* The first element added to the list will have id "item1"
-             * Subsequent list items will have IDs item2, item3, etc.
-             * Arguably this is too brittle, but rather than forcing you
-             * all to become experts on the DOM, you may assume this is done
-             * correctly, and/or you're testing this functionality implicitly. */
-            WebElement li = driver.findElement(By.id("item1"));
-            // We use startsWith because getText includes the text of the Delete button
-            assertTrue(li.getText().startsWith("Something to do"), "Checking correct text for added element");
+            for(int i = 0; i < MAX_ITEMS; i++) {
+                // Make up a to-do and click the "Add to list" button
+                input.sendKeys("To-do item " + (i+1));
+                addButton.click();
+                input.clear();
+            }
+
+            assertTrue(true);
+
+//            /* The first element added to the list will have id "item1"
+//             * Subsequent list items will have IDs item2, item3, etc.
+//             * Arguably this is too brittle, but rather than forcing you
+//             * all to become experts on the DOM, you may assume this is done
+//             * correctly, and/or you're testing this functionality implicitly. */
+//            WebElement li = driver.findElement(By.id("item1"));
+//            // We use startsWith because getText includes the text of the Delete button
+//            assertTrue(li.getText().startsWith("Something to do"), "Checking correct text for added element");
+
         } finally {
             driver.quit();
         }
